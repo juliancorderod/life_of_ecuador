8e6c231bfbcad4fdc68388f9d90125ad637df92c
Unknown <jc6984@nyu.edu>
2017-06-12 01:01:05 -0500

    added flower vase and clock model (with moving hand)

---
+++ Assets/_scripts/clockHandles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clockHandles : MonoBehaviour {
+
+	public GameObject clockHandle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		clockHandle.transform.Rotate(0f,1f *Time.deltaTime,0f);
+		
+	}
+}
