a0162c90ff5a5dcdc88576c662c7813a5af818a9
Unknown <jc6984@nyu.edu>
2017-06-13 13:42:20 -0500

    added scene transition

--- Assets/sceneTransition.cs
+++ Assets/sceneTransition.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class sceneTransition : MonoBehaviour {
 
+	public float transitionSpeed;
+
 	// Use this for initialization
 	void Start () {
+		GetComponent<Image>().color = Color.black;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		GetComponent<Image>().color -= new Color(0f,0f,0f,transitionSpeed * Time.deltaTime);
+
+
 		
 	}
 }
