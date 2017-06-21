49d34fb266cbc443d830d4632fdc8ef4f6123cd3
waiwha <rag452@nyu.edu>
2017-06-20 21:18:20 -0500

    branches + central pillar(?)

--- Assets/_scripts/v1/Branch.cs
+++ Assets/_scripts/v1/Branch.cs
@@ -18,6 +18,9 @@ public class Branch : MonoBehaviour {
 
 	public Sprite[] _pos_leaves;
 
+	[Range(0f,1f)]
+	public float _leaf_alpha;
+
 	// Use this for initialization
 	void Start () {
 		_num_leaves = (int)(_percent * _num_leaves);
@@ -49,6 +52,7 @@ public class Branch : MonoBehaviour {
 			_new_Leaf.transform.localScale *= Random.Range (1f, _leaf_scale);
 			_new_Leaf.transform.eulerAngles = Random.insideUnitSphere*360f;
 			_new_Leaf.GetComponent<SpriteRenderer> ().sprite = _pos_leaves [Random.Range (0, _pos_leaves.Length)];
+			_new_Leaf.GetComponent<SpriteRenderer> ().color = new Color ((187f / 255f), 1f, Random.Range (140, 250) / 255f, _leaf_alpha);
 		}
 	}
 }
