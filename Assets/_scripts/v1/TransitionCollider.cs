b1630f752cb2bb69bebfd667fc9c62d7a04ddd19
waiwha <rag452@nyu.edu>
2017-06-21 18:04:05 -0500

    optimizing

---
+++ Assets/_scripts/v1/TransitionCollider.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionCollider : MonoBehaviour {
+	public string _name;
+
+	public GameObject _linkedRoom;
+	public GameObject _linkedCollider;
+
+	public bool _active;
+
+	void Start(){
+		if (_linkedRoom.activeSelf)
+			_active = true;
+		else
+			_active = false;
+	}
+
+	void Update(){
+		if (_active) {
+			if (!_linkedRoom.activeSelf)
+				_linkedRoom.SetActive (true);
+		} else {
+			if (_linkedRoom.activeSelf)
+				_linkedRoom.SetActive (false);
+		}
+	}
+		
+	public bool GetState(){
+		return _active;
+	}
+
+	public void SetState(bool b){
+		_active = b;
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.name == "player") {
+			if(!_active)
+				_active = true;
+
+			if (_linkedCollider.GetComponent<TransitionCollider> ().GetState ())
+				_linkedCollider.GetComponent<TransitionCollider> ().SetState (false);
+		}
+	}
+}
