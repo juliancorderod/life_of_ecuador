db38bd20d2d17f802a2b55c2dcc7ce5e292626c2
waiwha <rag452@nyu.edu>
2017-06-27 18:34:17 -0500

    leaf-hands

--- Assets/_scripts/v2/spawnPeople.cs
+++ Assets/_scripts/v2/spawnPeople.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class spawnPeople : MonoBehaviour {
+	public GameObject pr_rotator;
+
+	[Range(0, 25)]
+	public int _numRotators;
+
+
 	private GameObject[] rotators;
 
 	// Use this for initialization
 	void Start () {
-		
+		rotators = new GameObject[_numRotators];
+
+		for (int i = 0; i < _numRotators; i++)
+			rotators [i] = Instantiate (pr_rotator, transform.position, transform.rotation, transform);
 	}
 	
 	// Update is called once per frame
