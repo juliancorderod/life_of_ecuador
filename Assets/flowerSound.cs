dcca96713e7a79da38840993c7e034add4329fde
Unknown <jc6984@nyu.edu>
2017-07-04 18:30:28 -0500

    uehd

--- Assets/flowerSound.cs
+++ Assets/flowerSound.cs
@@ -8,21 +8,26 @@ public class flowerSound : MonoBehaviour {
 	GameOfLife gameOfLife;
 	AudioSource audioSource;
 
+	public SpriteRenderer flowerSide1, flowerSide2;
+
 	// Use this for initialization
 	void Start () {
 
 		flowerController = GetComponent<FlowerController>();
 		gameOfLife = GetComponent<GameOfLife>();
 		audioSource = GetComponent<AudioSource>();
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		audioSource.pitch = gameOfLife.NUM_NEIGHBORS;
-
-		Debug.Log(gameOfLife.NUM_NEIGHBORS);
-
+		if(flowerController._inComputer){
+			audioSource.pitch = gameOfLife.NUM_NEIGHBORS;
+			audioSource.volume = 0.1f;
+		}else{
+			audioSource.pitch = gameOfLife.NUM_NEIGHBORS + Random.Range(0.5f,1.5f);
+			audioSource.volume = 0.5f;
+		}
 	}
 }
