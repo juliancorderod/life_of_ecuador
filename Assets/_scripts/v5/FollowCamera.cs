3bd8366e1578a073df19f46446e2e7befca82e52
waiwha <rag452@nyu.edu>
2017-07-21 00:42:10 -0500

    hey there

--- Assets/_scripts/v5/FollowCamera.cs
+++ Assets/_scripts/v5/FollowCamera.cs
@@ -5,27 +5,18 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour {
 	private GameObject _followedObj;
 
-	private int _on;
-
 	private float _move_Speed = 1f;
 
 
 
 	// Use this for initialization
 	void Start () {
-		//toggle camera on/off
-		_on = 2;
-
-
+		_followedObj = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.P)) {
-			_on *= -1;
-		}
-
-		GetComponent<Camera> ().depth = _on - 1;
+		GetComponent<Camera> ().nearClipPlane = Mathf.Max (.01f, Vector3.Distance (transform.position, _followedObj.transform.position) - 2f);
 	}
 
 
